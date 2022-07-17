c60fa9f1e5b64695ed83b1d937686db3ca5993fb
armandowni <56640858+armandowni@users.noreply.github.com>
2022-07-17 19:03:05 +0700

    update orm config,const and gitignore

--- api/ormconfig.ts
+++ api/ormconfig.ts
@@ -1,14 +1,15 @@
 import { DataSourceOptions } from "typeorm";
+import { DB_CONFIG_DATABASE, DB_CONFIG_HOST, DB_CONFIG_PASSWORD, DB_CONFIG_PORT, DB_CONFIG_USER } from "../static/const";
 import { entities } from "./src/db";
 
 export const config: DataSourceOptions = {
   name: "default",
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "",
-  database: "test",
+  host: DB_CONFIG_HOST,
+  port: DB_CONFIG_PORT,
+  username: DB_CONFIG_USER,
+  password: DB_CONFIG_PASSWORD,
+  database: DB_CONFIG_DATABASE,
   synchronize: false,
   logging: false,
   entities: entities,
--- static/const.ts
+++ static/const.ts
@@ -1,2 +1,8 @@
 export const SERVER_HOST = 'localhost'
 export const SERVER_PORT = 3000
+
+export const DB_CONFIG_USER = "postgres"
+export const DB_CONFIG_PORT = 5432
+export const DB_CONFIG_HOST = SERVER_HOST
+export const DB_CONFIG_PASSWORD = 'admin123'
+export const DB_CONFIG_DATABASE = 'testingdb'
