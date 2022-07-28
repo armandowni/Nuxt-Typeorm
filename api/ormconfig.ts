8c1d086164310e238fea18d5dd7ccb6d70839c73
armandowni <armando.nurimansyah@gmail.com>
2022-07-28 11:01:04 +0700

    update const and ormconfig

--- api/ormconfig.ts
+++ api/ormconfig.ts
@@ -1,5 +1,11 @@
 import { DataSourceOptions } from "typeorm";
-import { DB_CONFIG_DATABASE, DB_CONFIG_HOST, DB_CONFIG_PASSWORD, DB_CONFIG_PORT, DB_CONFIG_USER } from "../modules/const";
+import {
+  DB_CONFIG_DATABASE,
+  DB_CONFIG_HOST,
+  DB_CONFIG_PASSWORD,
+  DB_CONFIG_PORT,
+  DB_CONFIG_USER,
+} from "../modules/const";
 import { entities } from "./src/db";
 
 export const config: DataSourceOptions = {
@@ -10,8 +16,14 @@ export const config: DataSourceOptions = {
   username: DB_CONFIG_USER,
   password: DB_CONFIG_PASSWORD,
   database: DB_CONFIG_DATABASE,
-  synchronize: false,
+  synchronize: true,
   logging: false,
   entities: entities,
   migrations: ["src/migration/**/*.ts"],
+  ssl: true,
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
