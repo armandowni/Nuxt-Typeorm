fed3003343e8d2903d278ed01668e4f08987f0ea
armandowni <armando.nurimansyah@gmail.com>
2022-07-28 11:20:41 +0700

    change const

--- modules/const.ts
+++ modules/const.ts
@@ -1,17 +1,8 @@
-export const SERVER_HOST = process.env.HOST || 'localhost'
-export const SERVER_PORT = process.env.PORT ||3000
+export const SERVER_HOST = process.env.HOST || "localhost";
+export const SERVER_PORT = process.env.PORT || 3000;
 
-// development
-// export const DB_CONFIG_USER = "postgres"
-// export const DB_CONFIG_PORT = 5432
-// export const DB_CONFIG_HOST = SERVER_HOST
-// export const DB_CONFIG_PASSWORD = 'admin123'
-// export const DB_CONFIG_DATABASE = 'testingdb'
-
-
-// production
-export const DB_CONFIG_USER = "ndphgigmsldyrf"
-export const DB_CONFIG_PORT = 5432
-export const DB_CONFIG_HOST = "ec2-3-230-122-20.compute-1.amazonaws.com"
-export const DB_CONFIG_PASSWORD = '2f86ab68c36d3be06b46dd7c10bd1422ffa920303040c3081fb48e823b54f913'
-export const DB_CONFIG_DATABASE = 'd3guldico2husd'
+export const DB_CONFIG_USER = process.env.DB_USER || "postgres";
+export const DB_CONFIG_PORT = parseInt(process.env.DB_PORT) || 5432;
+export const DB_CONFIG_HOST = process.env.DB_HOST || SERVER_HOST;
+export const DB_CONFIG_PASSWORD = process.env.PASSWORD || "admin123";
+export const DB_CONFIG_DATABASE = process.env.DATABASE || "testingdb";
