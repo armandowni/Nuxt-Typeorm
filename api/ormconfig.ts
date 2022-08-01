06f5a41db9b4fee41d0231cecb7f420ce7261c0f
armandowni <armando.nurimansyah@gmail.com>
2022-08-01 10:27:49 +0700

    update nuxt and ormconfig

--- api/ormconfig.ts
+++ api/ormconfig.ts
@@ -2,21 +2,18 @@ import { DataSourceOptions } from "typeorm";
 import nuxtConfig from "../nuxt.config";
 import { entities } from "./src/db";
 
+const DB = nuxtConfig.env.DB;
 export const config: DataSourceOptions = {
   name: "default",
   type: "postgres",
-  username: "ndphgigmsldyrf",
   port: 5432,
-  host: "ec2-3-230-122-20.compute-1.amazonaws.com",
-  password: "2f86ab68c36d3be06b46dd7c10bd1422ffa920303040c3081fb48e823b54f913",
-  database: "d3guldico2husd",
+  username: DB.username || "postgres",
+  host: DB.host || "localhost",
+  password: DB.password || "admin123",
+  database: DB.database || "testingdb",
   synchronize: true,
   logging: false,
   entities: entities,
   migrations: ["src/migration/**/*.ts"],
-  extra: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  },
+  extra: !DB.database ? null : DB.extra,
 };
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,20 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
+  env: {
+    DB: {
+      host: "ec2-3-230-122-20.compute-1.amazonaws.com",
+      username: "ndphgigmsldyrf",
+      password: "2f86ab68c36d3be06b46dd7c10bd1422ffa920303040c3081fb48e823b54f913",
+      database: "d3guldico2husd",
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    },
+  },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
