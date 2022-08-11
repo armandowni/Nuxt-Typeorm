ba1d93c0bd44d7b4896bcff6401591d85cc82a19
armandowni <armando.nurimansyah@gmail.com>
2022-08-11 20:36:38 +0700

    update nuxt config dan vercel json

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,8 +57,8 @@ export default {
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  // modules: ["./modules/api"],
-  serverMiddleware: ['./api', './api/index'],
+  modules: ["./modules/api"],
+  // serverMiddleware: ['./api', './api/index'],
 
   watch: ["./api/express/routes"],
 };
