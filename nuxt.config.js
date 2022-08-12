3395b1ea2da941cdaac97087c5ac9e3736b2de40
armandowni <armando.nurimansyah@gmail.com>
2022-08-12 09:32:19 +0700

    update config and vercel

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,20 +13,19 @@ export default {
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
-
   env: {
     DB: {
       // config for db production
-      host: "ec2-3-230-122-20.compute-1.amazonaws.com",
-      username: "d3guldico2husd",
-      password: "ndphgigmsldyrf",
-      database:
-        "2f86ab68c36d3be06b46dd7c10bd1422ffa920303040c3081fb48e823b54f913",
-      extra: {
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
+      // host: "ec2-3-230-122-20.compute-1.amazonaws.com",
+      // username: "d3guldico2husd",
+      // password: "ndphgigmsldyrf",
+      // database:
+      //   "2f86ab68c36d3be06b46dd7c10bd1422ffa920303040c3081fb48e823b54f913",
+      // extra: {
+      //   ssl: {
+      //     rejectUnauthorized: false,
+      //   },
+      // },
     },
   },
 
