e460b6b4fe5cc716689d5e115e1436d4d743b462
Duckcanfly <duckcanfly@Duckcanflys-MacBook-Pro.local>
2023-10-30 10:33:10 +0700

    fixing

--- api/express/routes/sendMail/index.ts
+++ api/express/routes/sendMail/index.ts
@@ -4,6 +4,9 @@ export const post = async (req, res) => {
   const data = req.body;
   if (Object.keys(data).length == 0) throw new Error("data cannot empty");
 
-  const result = await sendMail(data);
-  res.send(result);
+  await sendMail(data)
+    .then((data) => {
+      res.status(200).send({ message: "Success Send Email" });
+    })
+    .catch((err) => new Error(err.message));
 };
