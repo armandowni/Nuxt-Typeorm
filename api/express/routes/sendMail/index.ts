773aa3a8801585590ea00b46b180f45d50739085
Duckcanfly <duckcanfly@Duckcanflys-MacBook-Pro.local>
2023-10-30 09:43:35 +0700

    adding send mail

---
+++ api/express/routes/sendMail/index.ts
@@ -0,0 +1,9 @@
+import { sendMail } from "~/api/util/mail";
+
+export const post = async (req, res) => {
+  const data = req.body;
+  if (Object.keys(data).length == 0) throw new Error("data cannot empty");
+
+  const result = await sendMail(data);
+  res.send(result);
+};
